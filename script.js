51a48da88312a8f99b899d809e7a1bd56b84bfd6
balkashbayb <balkashbaybakytzhan@gmail.com>
2023-01-24 20:59:21 +0500

    EXAM

---
+++ script.js
@@ -0,0 +1,46 @@
+//first//
+//const arr = [ "1", "43", "23", "54", "2", "3", "89"]
+//const str = arr.join(" ")
+//console.log(str)
+
+
+//second//
+//const arr = [ "1", "43", "23", "54", "2", "3", "89"]
+//let max = arr[0]
+//let min = arr[0]
+//for(let i = 0; i<arr.length; i++){
+//    if(max<arr[i]){
+//        max=arr[i]
+//    }
+//    if(min>arr[i]){
+//        min=arr[i]
+//    }
+//}
+//console.log (max)
+//console.log (min)
+
+//third//
+//const arr = [ 1, 43, 23, 54, 2, 3, 89]
+//let even = 0
+//for (let i=0;i<arr.length;i++){
+//    if (arr[i] % 2 === 0) {
+//       even+= arr[i];
+//  }
+//}
+//console.log(even)
+
+//four//
+//const arr = [ "1", "43", "23", "54", "2", "3", "89"]
+//for (let i = arr.length - 1; i >= 0; i--) {
+//    console.log(arr[i])
+//}
+
+//five//
+//const arr = [ "1", "43", "23", "54", "2", "3", "89"]
+//const number = arr.join(",")
+//console.log(number)
+
+//six//
+
+
+
